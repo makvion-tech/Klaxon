0b6034b5fb53f4eefe0e075c86fc7f3abf3d9269
Favour <georgechiamaka02@gmail.com>
2026-07-27 19:47:00 +0100

    Fix typo in productRoutes require path

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, getProductBySlug, createProduct, updateProduct, deleteProduct } = require('../controllerproductController');
+const { getAllProducts, getProductBySlug, createProduct, updateProduct, deleteProduct } = require('../controller/productController');
 const { protect } = require('../middleware/Authmiddleware');
 
 router.get('/', getAllProducts);
